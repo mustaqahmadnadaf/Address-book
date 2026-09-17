478bc306c09f773e1fa2e2ddc28066c447b4bfcc
Mustaq <mustaqahmadnadaf@gmail.com>
2026-09-17 19:37:09 +0530

    Fix formatting and variable initialization in file.c

--- file.c
+++ file.c
@@ -4,16 +4,16 @@
 //save all cnontacts to csv file
 void saveContactsToFile(AddressBook *addressBook) 
 {
-    FILE *fp=fopen("contact.csv","w");   //open the file in write mode
+    FILE *fp = fopen("contact.csv","w");   //open the file in write mode
 
-    if(fp==NULL)  //if fopen() fails, fp becomes NULL
+    if(fp == NULL)  //if fopen() fails, fp becomes NULL
     {
         printf("\33[31mError: Cannot open cantact.csv for writing\33[0m\n");
         return;    
     }
 
     //write each contact in csv format: name, phone or email.
-    for(int i=0;i<addressBook->contactCount;i++)
+    for(int i = 0;i < addressBook->contactCount;i++)
     {
         fprintf(fp,"%s,%s,%s\n",addressBook->contacts[i].name,
                                 addressBook->contacts[i].phone,
@@ -27,15 +27,15 @@ void saveContactsToFile(AddressBook *addressBook)
 //load all contacts from csv file
 void loadContactsFromFile(AddressBook *addressBook) 
 {
-    FILE *fp=fopen("contact.csv","r");   //open file for reading
+    FILE *fp = fopen("contact.csv","r");   //open file for reading
     
-    if(fp==NULL) //if fopen() fails,fp becomes NULL
+    if(fp == NULL) //if fopen() fails,fp becomes NULL
     {
         printf("\33[31mError: Cannot open cantact.csv for reading\33[0m\n");
         return;
     }
 
-    addressBook->contactCount=0;  //eset contact count
+    addressBook->contactCount = 0;  //eset contact count
 
     //read until end of file
     while(!feof(fp))     //loop until EoF
@@ -44,7 +44,7 @@ void loadContactsFromFile(AddressBook *addressBook)
         break;
 
         //point to next contact structure
-        Contact *c=&addressBook->contacts[addressBook->contactCount];
+        Contact *c = &addressBook->contacts[addressBook->contactCount];
 
         //read one line in csv format:name until cama, number until coma or email until new line
         if(fscanf(fp,"%[^,],%[^,],%[^\n]\n",c->name,c->phone,c->email)==3)
